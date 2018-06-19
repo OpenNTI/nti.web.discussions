d7fd46414eab36c3dc960f27f83ce9ea7d127f40
Josh Birdwell <jbirdwell34@gmail.com>
2018-06-19 13:38:58 -0500

    NTI-5970: adding empty state for topic list

--- src/forums/index.js
+++ src/forums/index.js
@@ -1,3 +1,4 @@
 export TopicParticipationSummary from './topic-participation-summary';
 export DiscussionSelectionEditor from './discussion-selection';
 export ForumCreate from './create';
+export EmptyTopicList from './components/EmptyTopicList';
