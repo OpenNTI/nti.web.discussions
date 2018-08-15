2553bac59da593dd93630fa2948dd000df50ed40
Jonathan Grimes <jsg2021@gmail.com>
2018-08-15 12:51:12 -0500

    update test harness

--- test/app/index.js
+++ test/app/index.js
@@ -3,10 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Forums} from '../../src';
 
-import 'normalize.css';
-import '@nti/style-common/all.scss';
-import '@nti/web-commons/lib/index.css';
-import '@nti/web-modeled-content/lib/index.css';
 
 window.$AppConfig = window.$AppConfig || {server: '/dataserver2/'};
 
