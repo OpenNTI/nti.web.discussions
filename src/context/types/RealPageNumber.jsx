3e9542f7ae73b42d281e92bbfda371649180ded6
Josh Birdwell <jbirdwell34@gmail.com>
2018-08-28 14:35:04 -0500

    NTI-6503: Real Page Number

---
+++ src/context/types/RealPageNumber.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Registry from './Registry';
+
+export default
+@Registry.register('realpagenumber')
+class RealPageNumber extends React.PureComponent {
+	render () {
+		return null;
+	}
+}
