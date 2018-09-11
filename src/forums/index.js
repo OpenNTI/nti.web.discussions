597381d863a848605f7f74e1a45f3ac3fd741f4a
Josh Birdwell <jbirdwell34@gmail.com>
2018-09-11 12:36:12 -0500

    Start change to forum editor

--- src/forums/index.js
+++ src/forums/index.js
@@ -1,5 +1,4 @@
 export TopicParticipationSummary from './topic-participation-summary';
 export DiscussionSelectionEditor from './discussion-selection';
-export ForumCreate from './create';
 export EmptyTopicList from './components/EmptyTopicList';
 export ForumList from './forum-list';
