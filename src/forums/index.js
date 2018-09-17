4bd6fb25b3b510f1875a0c95c6e54a5d8d4232bf
Josh Birdwell <jbirdwell34@gmail.com>
2018-09-17 14:41:16 -0500

    Use the save cancel dialog

--- src/forums/index.js
+++ src/forums/index.js
@@ -1,4 +1,4 @@
 export TopicParticipationSummary from './topic-participation-summary';
 export DiscussionSelectionEditor from './discussion-selection';
 export EmptyTopicList from './components/EmptyTopicList';
-export ForumList from './forum-list';
+export * from './forum-list';
