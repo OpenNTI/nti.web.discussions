d715bf84b9de0798b4d4dbc316dd75a2cc16de14
Josh Birdwell <jbirdwell34@gmail.com>
2018-09-19 14:28:33 -0500

    Add more snapshot tests

--- src/forums/forum-list/__test__/ForumItem.spec.js
+++ src/forums/forum-list/__test__/ForumItem.spec.js
@@ -4,28 +4,16 @@ import renderer from 'react-test-renderer';
 
 import ForumItem from '../ForumItem';
 
-const onBefore = () => {
-	jest.useFakeTimers();
-};
-
-const flushPromises = () => new Promise(resolve => setImmediate(resolve));
-
 describe('Forum Item', () => {
-	beforeEach(onBefore);
-
 	test('No Discussions - Snapshot', async () => {
 		const item = {
 			title: 'Forum',
 			TopicCount: 0,
-			getRecentActivity: () => Promise.resolve({ TotalItemCount: 0, Items: [] }),
 			hasLink: () => { return false; },
-			edit: () => {}
+			edit: () => {},
+			Links: []
 		};
 		const itemCmp = renderer.create(<ForumItem item={item} />);
-
-		jest.runAllTimers();
-		await flushPromises();
-
 		const tree = itemCmp.toJSON();
 		expect(tree).toMatchSnapshot();
 	});
