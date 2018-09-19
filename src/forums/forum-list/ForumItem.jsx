22182e581f119472e1f00ffaa407596bbb5c237d
Cory Jones <cory.jones@nextthought.com>
2018-09-19 11:30:44 -0500

    Remove edit from forum items, export forum editor

--- src/forums/forum-list/ForumItem.jsx
+++ src/forums/forum-list/ForumItem.jsx
@@ -3,10 +3,6 @@ import PropTypes from 'prop-types';
 import { LinkTo } from '@nti/web-routing';
 import { HOC } from '@nti/web-commons';
 import { scoped } from '@nti/lib-locale';
-import cx from 'classnames';
-import { encodeForURI } from '@nti/lib-ntiids';
-
-import Editor from '../forum-editor';
 
 const DEFAULT_TEXT = {
 	count: {
@@ -42,20 +38,8 @@ class ForumItem extends React.Component {
 		showEditor: false
 	}
 
-	onSubmit = (payload) => {
-		this.props.item.edit(payload);
-		this.setState({ showEditor: false });
-	}
-
-	toggleEditor = (e) => {
-		e.preventDefault();
-		this.setState({ showEditor: !this.state.showEditor });
-	}
-
 	render () {
-		const { showEditor } = this.state;
 		const { item } = this.props;
-		const canEdit = item.hasLink('edit');
 
 		return (
 			<li className="forum-item-li">
@@ -67,14 +51,8 @@ class ForumItem extends React.Component {
 								<span className="see-all count">{t('count', { count: item.TopicCount })}</span>
 							</div>
 						</div>
-						{canEdit && (
-							<div className="forum-item-edit" onClick={this.toggleEditor}>
-								<i className="icon-edit" />
-							</div>
-						)}
 					</div>
 				</LinkTo.Object>
-				{showEditor && <Editor title={item.title} onSubmit={this.onSubmit} onBeforeDismiss={this.toggleEditor} isEditing />}
 			</li>
 		);
 	}
--- src/forums/index.js
+++ src/forums/index.js
@@ -2,3 +2,4 @@ export TopicParticipationSummary from './topic-participation-summary';
 export DiscussionSelectionEditor from './discussion-selection';
 export EmptyTopicList from './components/EmptyTopicList';
 export * from './forum-list';
+export Editor from './forum-editor';
