f41691fc220f11a99d2dd4e9c9427e740a2965d5
Cory Jones <cory.jones@nextthought.com>
2018-09-24 09:53:58 -0500

    NTI-6691 Expose binDiscussions

--- src/forums/forum-list/index.js
+++ src/forums/forum-list/index.js
@@ -3,6 +3,7 @@ import { Router, Route } from '@nti/web-routing';
 import ForumListView from './View';
 
 export * from './constants';
+export * from './utils';
 
 export const ForumList = Router.for([
 	Route({
