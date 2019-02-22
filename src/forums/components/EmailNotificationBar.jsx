0c6687c056806ac00478da2b975b4d6c51e49638
Andrew Ligon <andrew.ligon@nextthought.com>
2019-02-21 22:37:33 -0600

    NTI-7443 NTI-7490 add a component for messaging when a topic will create an email

---
+++ src/forums/components/EmailNotificationBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Panels} from '@nti/web-commons';
+import {scoped} from '@nti/lib-locale';
+
+const t = scoped('discussions.forums.EmailNotificationBar', {
+	message: 'Creating a discussion in this forum will notify the community.'
+});
+
+export default class EmailNotificationBar extends React.Component {
+	render () {
+		return (
+			<Panels.MessageBar iconCls="icon-bell" message={t('message')} />
+		);
+	}
+}
--- src/forums/index.js
+++ src/forums/index.js
@@ -1,5 +1,6 @@
 export TopicParticipationSummary from './topic-participation-summary';
 export DiscussionSelectionEditor from './discussion-selection';
 export EmptyTopicList from './components/EmptyTopicList';
+export EmailNotificationBar from './components/EmailNotificationBar';
 export * from './forum-list';
 export Editor from './forum-editor';
