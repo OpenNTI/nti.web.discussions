c0d3ce2721de4f80901951f68aa9ff17524091e2
Andrew Ligon <andrew.ligon@nextthought.com>
2019-04-08 18:00:32 -0500

    Update the note summary card when the note updates

--- src/notes/sidebar/summary/View.jsx
+++ src/notes/sidebar/summary/View.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {DisplayName} from '@nti/web-commons';
+import {Hooks, Events} from '@nti/web-session';
 import classnames from 'classnames/bind';
 
 import NoteMetadata from './NoteMetadata';
@@ -9,16 +10,34 @@ import styles from './View.css';
 
 const cx = classnames.bind(styles);
 
-export default function NoteSummary ({note, ...props}) {
-	return (
-		<div className={cx('note-summary')}>
-			<DisplayName className={cx('author')} entity={note.creator} />
-			<NotePreview note={note} />
-			<NoteMetadata note={note} />
-		</div>
-	);
-}
+export default
+@Hooks.onEvent(Events.NOTE_UPDATED, 'noteUpdated')
+class NoteSummary extends React.Component {
+	static propTypes = {
+		note: PropTypes.object.isRequired
+	}
 
-NoteSummary.propTypes = {
-	note: PropTypes.object.isRequired
-};
+
+	noteUpdated (updated) {
+		const {note} = this.props;
+
+		if (note === updated) {
+			this.forceUpdate();
+		} else if (note.getID() === updated.getID()) {
+			note.refresh(updated.toJSON()).then(() => this.forceUpdate());
+		}
+	}
+
+
+	render () {
+		const {note} = this.props;
+
+		return (
+			<div className={cx('note-summary')}>
+				<DisplayName className={cx('author')} entity={note.creator} />
+				<NotePreview note={note} />
+				<NoteMetadata note={note} />
+			</div>
+		);
+	}
+}
