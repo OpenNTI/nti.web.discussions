13889e4bda529392a9ef44f8e1e461bf17d92af5
Andrew Ligon <andrew.ligon@nextthought.com>
2019-08-28 10:49:49 -0500

    Use the common report launch button component

--- src/item/common/Report.jsx
+++ src/item/common/Report.jsx
@@ -1,38 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames/bind';
-import {Viewer} from '@nti/web-reports';
+import {Launch} from '@nti/web-reports';
 
-import Styles from './Report.css';
+DiscussionItemReport.propTypes = {
+	post: PropTypes.shape({
+		getReport: PropTypes.func
+	})
+};
+export default function DiscussionItemReport ({post, ...otherProps}) {
+	const report = post && post.getReport && post.getReport();
 
-const cx = classnames.bind(Styles);
-
-export default class DiscussionItemReport extends React.Component {
-	static propTypes = {
-		className: PropTypes.string,
-		post: PropTypes.shape({
-			getReport: PropTypes.func
-		})
-	}
-
-	launchReport = (e) => {
-		e.stopPropagation();
-		e.preventDefault();
-
-		const {post} = this.props;
-
-		Viewer.show(post.getReport());
-	}
-
-	render () {
-		const {className, post} = this.props;
-
-		if (!post || !post.getReport || !post.getReport()) { return null; }
-
-		return (
-			<button type="button" className={cx('discussion-item-report', className)} onClick={this.launchReport}>
-				<i className={cx('icon-report', 'icon')} />
-			</button>
-		);
-	}
+	return (
+		<Launch.Button report={report} {...otherProps} />	
+	);
 }
