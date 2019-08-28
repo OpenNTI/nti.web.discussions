a7edcc80f71d1d9d2abe6630c1fe1b8fe29444d5
Andrew Ligon <andrew.ligon@nextthought.com>
2019-08-28 13:20:47 -0500

    NTI-8282 update the comment count when adding or deleting comments

--- src/item/common/CommentCount.jsx
+++ src/item/common/CommentCount.jsx
@@ -14,16 +14,91 @@ const t = scoped('nti-discussion.item.common.CommentCount', {
 	}
 });
 
-DiscussionItemCommentCount.propTypes = {
-	className: PropTypes.string,
-	post: PropTypes.shape({
-		commentCount: PropTypes.number
-	})
-};
-export default function DiscussionItemCommentCount ({className, post}) {
-	return (
-		<Text.Base className={cx('discussion-item-comment-count', className)}>
-			{t('comments', {count: post.commentCount || 0})}
-		</Text.Base>
-	);
+export default class DiscussionItemCommentCount extends React.Component {
+	static propTypes = {
+		className: PropTypes.string,
+		post: PropTypes.shape({
+			getID: PropTypes.string,
+			commentCount: PropTypes.number,
+			addCommentAddedListener: PropTypes.func,
+			addCommentDeletedListener: PropTypes.func
+		})
+	}
+
+	state = {delta: 0}
+
+	componentDidMount () {
+		this.setup();
+	}
+
+	componentWillUnmount () {
+		this.cleanupListeners();
+	}
+
+	componentDidUpdate (prevProps) {
+		const {post} = this.props;
+		const {post:prevPost} = prevProps;
+
+		if (post.getID() !== prevPost.getID()) {
+			this.setup();
+		}
+	}
+
+
+	addListeners () {
+		this.cleanupListeners();
+
+		const {post} = this.props;
+
+		if (post.addCommentAddedListener) {
+			this.cleanupCommentAddedListener = post.addCommentAddedListener((newComment) => {
+				const {delta} = this.state;
+
+				this.setState({
+					delta: delta + 1
+				});
+			});
+		}
+
+		if (post.addCommentDeletedListener) {
+			this.cleanupCommentDeletedListener = post.addCommentDeletedListener((deletedComment) => {
+				const {delta} = this.state;
+
+				this.setState({
+					delta: delta - 1
+				});
+			});
+		}
+	}
+
+
+	cleanupListeners () {
+		if (this.cleanupCommentAddedListener) {
+			this.cleanupCommentAddedListener();
+			delete this.cleanupCommentAddedListener;
+		}
+
+		if (this.cleanupCommentDeletedListener) {
+			this.cleanupCommentDeletedListener();
+			delete this.cleanupCommentDeletedListener;
+		}
+	}
+
+
+	setup () {
+		this.addListeners();
+	}
+
+	render () {
+		const {className, post} = this.props;
+		const {delta} = this.state;
+
+		const count = (post.commentCount || 0) + (delta || 0);
+
+		return (
+			<Text.Base className={cx('discussion-item-comment-count', className)}>
+				{t('comments', {count})}
+			</Text.Base>
+		);
+	}
 }
