195a3e2e38e4821628f83898ce1abd81c8f72390
Andrew Ligon <andrew.ligon@nextthought.com>
2020-06-05 22:23:51 -0500

    add a title input

--- src/editor/Editor.jsx
+++ src/editor/Editor.jsx
@@ -4,6 +4,7 @@ import {Editor} from '@nti/web-modeled-content';
 
 import Container from './parts/Container';
 import Identity from './parts/Identity';
+import Title from './parts/Title';
 
 DiscussionEditor.propTypes = {
 	className: PropTypes.string,
@@ -13,10 +14,17 @@ DiscussionEditor.propTypes = {
 	})
 };
 export default function DiscussionEditor ({className, discussion}) {
+	const [title, setTitle] = React.useState(null);
+
+	React.useEffect(() => {
+		setTitle(discussion?.title);
+	}, [discussion]);
+
 	return (
 		<Editor.ContextProvider>
 			<Container>
-				<Identity creator={discussion.Creator} />
+				<Identity creator={discussion?.Creator} />
+				<Title title={title} onChange={setTitle} />
 			</Container>
 		</Editor.ContextProvider>
 	);
---
+++ src/editor/parts/Title.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames/bind';
+import {scoped} from '@nti/lib-locale';
+import {Editor, Plugins, Parsers, ContextProvider} from '@nti/web-editor';
+
+import Styles from './Styles.css';
+
+const cx = classnames.bind(Styles);
+const t = scoped('nti-discussions.editor.parts.Title', {
+	placeholder: 'Title...'
+});
+
+const toDraftState = value => Parsers.PlainText.toDraftState(value);
+const fromDraftState = draftState => Parsers.PlainText.fromDraftState(draftState);
+
+const {CharacterCounter} = Plugins.Counter.components;
+const EditorPlugins = [
+	Plugins.Plaintext.create(),
+	Plugins.SingleLine.create(),
+	Plugins.Counter.create({character: {limit: 140}})
+];
+
+DiscussionEditorTitle.propTypes = {
+	title: PropTypes.string,
+	onChange: PropTypes.func
+};
+export default function DiscussionEditorTitle ({title, onChange}) {
+	const [editor, setEditor] = React.useState(null);
+	const setEditorRef = (ref) => {
+		if (ref !== editor) { setEditor(ref); }
+	};
+
+	const titleRef = React.useRef(null);
+	const [editorState, setEditorState] = React.useState(null);
+
+	React.useEffect(() => {
+		if (!titleRef.current || title !== titleRef.current) {
+			setEditorState(toDraftState(title));
+		}
+	}, [title]);
+
+
+	const onContentChange = (newEditorState) => {
+		const newTitle = fromDraftState(newEditorState);
+
+		titleRef.current = newTitle;
+		onChange?.(newTitle);
+	};
+
+	return (
+		<div className={cx('title')}>
+			{editorState && (
+				<Editor
+					ref={setEditorRef}
+					plugins={EditorPlugins}
+					placeholder={t('placeholder')}
+					editorState={editorState}
+					onContentChange={onContentChange}
+				/>
+			)}
+			<ContextProvider editor={editor}>
+				<CharacterCounter className={cx('title-limit')} showLimit/>
+			</ContextProvider>
+		</div>
+	);
+}
