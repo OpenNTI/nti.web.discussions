6231d8b0e1a8f18b65fa3585f68261e867287db0
Andrew Ligon <andrew.ligon@nextthought.com>
2020-06-26 02:10:52 -0500

    add dialog styles to the discussion viewer

--- src/viewer/Viewer.jsx
+++ src/viewer/Viewer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
+import {scoped} from '@nti/lib-locale';
+import {StandardUI} from '@nti/web-commons';
 
 import Styles from './Viewer.css';
 import Body from './body';
@@ -8,23 +10,49 @@ import Post from './post';
 import Comments from './comments';
 
 const cx = classnames.bind(Styles);
+const t = scoped('nti-discussions.viewer.Viewer', {
+	dialog: {
+		title: 'Posted in %(title)s',
+		noTitle: 'Discussion'
+	}
+});
+
+const getDialogTitle = (discussion) => {
+	if (!discussion?.ContainerTitle) { return t('dialog.noTitle'); }
+
+	return t('dialog.title', {title: discussion.ContainerTitle});
+};
 
 DiscussionViewer.Body = Body;
 DiscussionViewer.propTypes = {
 	className: PropTypes.string,
-	discussion: PropTypes.object,
+	discussion: PropTypes.shape({
+		ContainerTitle: PropTypes.string
+	}),
 	container: PropTypes.oneOfType([
 		PropTypes.object,
 		PropTypes.array
 	]),
 
-	dialog: PropTypes.bool
+	dialog: PropTypes.bool,
+	onClose: PropTypes.func
 };
-export default function DiscussionViewer ({className, discussion, container, dialog}) {
+export default function DiscussionViewer ({className, discussion, container, dialog, onClose}) {
+	const Cmp = dialog ? StandardUI.Card : 'article';
+
 	return (
-		<article className={cx('discussion-viewer', className, {dialog})}>
+		<Cmp
+			{...(dialog ? ({as: 'article', rounded: true}) : ({}))}
+			className={cx('discussion-viewer', className, {dialog})}
+		>
+			{dialog && (
+				<StandardUI.Window.TitleBar
+					onClose={onClose}
+					title={getDialogTitle(discussion)}
+				/>
+			)}
 			<Post post={discussion} container={container} />
 			<Comments post={discussion} container={container} />
-		</article>
+		</Cmp>
 	);
 }
